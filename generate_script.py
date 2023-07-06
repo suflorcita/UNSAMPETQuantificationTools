b4273fb678594c033291695efbb63b9880b3cdec
Sol Cataldo <solcataldo98@gmail.com>
2023-07-06 14:35:38 -0300

    Update generate_script

--- generate_script.py
+++ generate_script.py
@@ -1,11 +1,12 @@
 import os
-def write_line_quantification(subject, RMN_path, FDG_path, path_file):
+def write_line_quantification(subject, RMN_path, FDG_path, output_path, path_file):
     with open(path_file, 'a') as f:
         f.write(
             f'python3 main.py '
             f'{FDG_path} '
             f'{RMN_path} '
-            f'{subject} \n'
+            f'{subject} '
+            f'{output_path}\n'
         )
     pass
 
@@ -43,7 +44,7 @@ def generate_script_pet_quant(output_directory, script_path):
             # PET path
             PET_path = nifti_subject + "/FDG-PET"
 
-            write_line_quantification(subject, RMN_path, PET_path, script_path)
+            write_line_quantification(subject, RMN_path, PET_path,output_directory, script_path)
 
 def generate_script_charts(output_directory, script_path):
     files = sorted(os.listdir(output_directory))
@@ -71,15 +72,15 @@ def generate_script_charts(output_directory, script_path):
 
 if __name__ == '__main__':
 
-    output_directory = "/home/sol/PET_MRI/Procesado/Procesar"
-    output_directory_chart = "/home/sol/PET_MRI/Procesado"
+    output_directory = "/home/sol/PET_MRI/Procesamiento/Procesar"
+    output_directory_chart = "/home/sol/PET_MRI/Procesamiento"
 
     # create a script to run FreeSurfer
     script_path_quant = "/home/sol/PET_MRI/quantification.sh"
     script_path_chart = "/home/sol/PET_MRI/charts.sh"
 
     generate_script_pet_quant(output_directory, script_path_quant)
-    generate_script_charts(output_directory_chart, script_path_chart)
+    #generate_script_charts(output_directory_chart, script_path_chart)
 
 
 
